2ef9ecf7d3588e6dbf383034e57af3b9a7b75163
Kent Chen <kchen21@nait.ca>
2022-09-16 11:18:46 -0600

    NhlSystem

---
+++ NhlSystem/NhlSystem/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhlSystem
+{
+    public class Person
+    {
+        // Define a fully-implemented property for FullName with a private set
+
+        private string _fullname = string.Empty; // Define a backing field for the FullName property
+        public string FullName
+        {
+            get { return _fullname; }
+            private set 
+            { 
+                // Validate new value is not null or empty string or whitespaces only 
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("FullName value is required");
+                }
+                // Validate new value contains at minimum 3 or more characters
+                else if (value.Trim().Length < 3)
+                { 
+                    throw new ArgumentException("FullName must contain 3 or more characters");
+                }
+                _fullname = value.Trim(); // Remove leading and trailing whitespaces 
+            }
+        }
+
+        // Create a greedy constructor with a parameter for the fullName
+        public Person(string fullName)
+        {
+            FullName = fullName;
+        }
+
+        public override string ToString()
+        {
+            return (FullName);
+        }
+    }
+}
---
+++ NhlSystem/NhlSystem/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*  Test Plan for Perosn
+ *  
+ *  Test Case                               Test Data                   Expected Result/Behaviour
+ *  ---------                               ---------                   -------------------------
+ *  Valid FullName                          FullName: Connor McDavid    FullName = Connor McDavid
+ *  
+ *  Null FullName                           FullName = null             ArgumentNullException
+ *  
+ *  Empty FullName                          FullName = ""               ArgumentNullException
+ *  
+ *  Whitespace FullName                     FullName = "   "            ArgumentNullException
+ *      
+ *  FullName less than 3 characters         FullName = "AB"             ArgumentException
+ * */
+
+// Test Case 1: Valid FullName
+using NhlSystem;
+
+var validPerson = new Person("Connor McDavid");
+Console.WriteLine(validPerson.FullName);    // The value should be Connor McDavid
+
+// Test Case 2: Null FullName
+try
+{
+    var nullPerson = new Person(null);
+    Console.WriteLine("Null Test Case failed.");
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine(ex.ParamName); // The output should be "FullName is required"
+}
+
+// Test Case 3: Empty FullName
+try
+{
+    var emptyPerson = new Person("");
+    Console.WriteLine("Empty FullName Test Case failed.");
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine(ex.ParamName); // The output should be "FullName is required"
+}
+
+// Test Case 4: Whitespace FullName
+try
+{
+    var whiteSpacePerson = new Person("    ");
+    Console.WriteLine("Empty FullName Test Case failed.");
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine(ex.ParamName); // The output should be "FullName is required"
+}
+
+// Test Case 5: Minimum Length FullName
+try
+{
+    var invalidFullNameLengthPerson = new Person("ab");
+    Console.WriteLine("Empty FullName Test Case failed.");
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message); // The output should be "FullName is required"
+}
